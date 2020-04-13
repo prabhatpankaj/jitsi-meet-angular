d27728dc860b0a7127cd40a70beb6b9a1bfc5a70
prabhat pankaj <pankajiitbhu@gmail.com>
2020-04-13 07:29:20 +0530

    working on jitsi meeting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,116 +8,57 @@ declare var $: any;
 })
 export class AppComponent implements OnInit {
   title = 'jitsi-meet-angular';
-  private jitsi: any;
-  private connection: any;
-  private room: any;
+  jitsi: any;
 
   constructor() {
     this.jitsi = (window as any).JitsiMeetJS;
   }
 
 
-  private initOptions = {
-    disableAudioLevels: true,
+  role = 'user';
 
-    desktopSharingChromeExtId: null,
-  
-    desktopSharingChromeDisabled: false,
-  
-    desktopSharingChromeSources: ['screen', 'window', 'tab'],
-  
-    desktopSharingChromeMinExtVersion: '0.1'
+  conferenceId = 'prabhatpankaj';
+  conferencePassword = 'pass1234';
 
-  };
 
-  private confOptions = {
-    openBridgeChannel: true
-  }
+  wait = (seconds) => new Promise(res => setTimeout(res, seconds * 1000));
 
-  private options = {
+
+  optionsJitsi = {
     hosts: {
       domain: 'beta.meet.jit.si',
-      muc: 'conference.beta.meet.jit.si' // FIXME: use XEP-0030
+      muc: 'conference.beta.meet.jit.si'
     },
-    bosh: 'https://beta.meet.jit.si/http-bind', // FIXME: use xep-0156 for that
-  
-    // The name of client node advertised in XEP-0115 'c' stanza
-    clientNode: 'http://jitsi.org/jitsimeet'
+    bosh: 'https://beta.meet.jit.si/http-bind',
+
+    clientNode: 'https://jitsi.org/jitsimeet'
   };
 
-  role = 'user';
+  confOptions = {
+    openBridgeChannel: true
+  };
+
+  initOptions = {
+    disableAudioLevels: true,
+    desktopSharingChromeExtId: null,
+    desktopSharingChromeDisabled: false,
+    desktopSharingChromeSources: ['screen', 'window', 'tab'],
+    desktopSharingChromeMinExtVersion: '0.1',
+  };
+
+  connection = null;
+  isJoined = false;
+  room = null;
+  isVideo: boolean = true;
+
   localTracks = [];
-
-  private createConnection(options): any {
-    return new this.jitsi.JitsiConnection(null, null, options);
-  }
-
-  private setConnectionListeners(connection: any): void {
-    connection.addEventListener(this.jitsi.events.connection.CONNECTION_ESTABLISHED, this.onConnectionSuccess);
-    connection.addEventListener(this.jitsi.events.connection.CONNECTION_FAILED, this.onConnectionFailed);
-    connection.addEventListener(this.jitsi.events.connection.CONNECTION_DISCONNECTED, this.disconnect);
-    this.jitsi.mediaDevices.addEventListener(this.jitsi.events.mediaDevices.DEVICE_LIST_CHANGED, this.onDeviceListChanged);
-    connection.connect();
-  }
-
-  private createRoom(connection: any, options: any) : void {
-    this.room = connection.initJitsiConference('conference', options);
-  }
-
-  private setRoomListeners(room: any): void {
-    room.on(this.jitsi.events.conference.TRACK_ADDED, this.onRemoteTrack);
-    room.on(this.jitsi.events.conference.CONFERENCE_JOINED, this.onConferenceJoined);
-  }
-
-  private onConnectionSuccess(): void {
-    console.log("onConnectionSuccess");
-  }
-
-  private onConnectionFailed(): void {
-    console.log("onConnectionFailed");
-  }
-
-  private disconnect(): void {
-    console.log('disconnecting?');
-  }
-
-  private onRemoteTrack(): void {
-    console.log("onRemoteTrack");
-  }
-
-  private onConferenceJoined(): void {
-    console.log("onConferenceJoined");
-  }
-
-  private onDeviceListChanged(devices): void {
-    console.info('current devices', devices);
-  }
-
-  startAsPresenter() {
-    this.role = 'presenter';
-    this.initializeVideo();
-  }
-
-  startAsUser() {
-    console.log("here")
-  }
-
-  quitConference() {
-    console.log("here")
-  }
-
-  switchVideo() {
-    console.log("here")
-  }
-
-  changeAudioOutput() {
-    console.log("here")
-  }
+  remoteTracks = {};
 
   onLocalTracks(tracks) {
     this.localTracks = tracks;
     for (let i = 0; i < this.localTracks.length; i++) {
-      this.localTracks[i].addEventListener(this.jitsi.events.track.TRACK_AUDIO_LEVEL_CHANGED,
+      this.localTracks[i].addEventListener(
+        this.jitsi.events.track.TRACK_AUDIO_LEVEL_CHANGED,
         audioLevel => console.log(`Audio Level local: ${audioLevel}`));
       this.localTracks[i].addEventListener(
         this.jitsi.events.track.TRACK_MUTE_CHANGED,
@@ -145,29 +86,262 @@ export class AppComponent implements OnInit {
           `<audio autoplay='1' muted='true' id='localAudio${i}' />`);
         this.localTracks[i].attach($(`#localAudio${i}`)[0]);
       }
+      if (this.isJoined) {
+        this.room.addTrack(this.localTracks[i]);
+      }
     }
   }
-  
 
-  async initializeVideo() {
+
+  onRemoteTrack(track) {
+    if (track.isLocal()) {
+      return;
+    }
+    const participant = track.getParticipantId();
+    const displayName = this.room.getParticipantById(participant).getDisplayName();
+    if (displayName &&
+      displayName !== 'presenter') {
+      return;
+    }
+
+    if (!this.remoteTracks[participant]) {
+      this.remoteTracks[participant] = [];
+    }
+    const idx = this.remoteTracks[participant].push(track);
+
+    track.addEventListener(
+      this.jitsi.events.track.TRACK_AUDIO_LEVEL_CHANGED,
+      audioLevel => console.log(`Audio Level remote: ${audioLevel}`));
+    track.addEventListener(
+      this.jitsi.events.track.TRACK_MUTE_CHANGED,
+      () => console.log('remote track muted'));
+    track.addEventListener(
+      this.jitsi.events.track.LOCAL_TRACK_STOPPED,
+      () => console.log('remote track stoped'));
+    track.addEventListener(this.jitsi.events.track.TRACK_AUDIO_OUTPUT_CHANGED,
+      deviceId =>
+        console.log(
+          `track audio output device was changed to ${deviceId}`));
+
+    const divId = participant + track.getType() + idx;
+
+    if (track.getType() === 'video') {
+      const width = displayName === 'presenter' ? 300 : 100;
+      const videoDiv = `
+        <div class='${participant}'>
+          <div style="font-size: 100px;">
+            Remote video from ${displayName}
+          </div>
+          <video style="width: ${width}px;" autoplay='1' id='${divId}' />
+        </div>
+      `;
+      $('body').append(videoDiv);
+    } else {
+      $('body').append(
+        `<audio autoplay='1' id='${divId}' />`);
+    }
+
+    track.attach($(`#${divId}`)[0]);
+  }
+
+  onConferenceJoined() {
+    console.log('conference joined!');
+    this.isJoined = true;
+    for (let i = 0; i < this.localTracks.length; i++) {
+      this.room.addTrack(this.localTracks[i]);
+    }
+  }
+
+
+  onUserLeft(id) {
+    console.log('user left');
+    if (!this.remoteTracks[id]) {
+      return;
+    }
+    const tracks = this.remoteTracks[id];
+
+    for (let i = 0; i < tracks.length; i++) {
+      tracks[i].detach($(`#${id}${tracks[i].getType()}`));
+    }
+  }
+
+  onConnectionSuccess = async () => {
+    this.room = this.connection.initJitsiConference(this.conferenceId, this.confOptions);
+    this.room.setDisplayName(this.role);
+    this.room.on(this.jitsi.events.conference.TRACK_ADDED, this.onRemoteTrack);
+    this.room.on(this.jitsi.events.conference.TRACK_REMOVED, track => {
+      const participant = track.getParticipantId();
+      $(`.${participant}`).remove();
+      console.log(`track removed!!!${track}`);
+    });
+    this.room.on(
+      this.jitsi.events.conference.CONFERENCE_JOINED,
+      this.onConferenceJoined);
+    this.room.on(this.jitsi.events.conference.USER_JOINED, (id, user) => {
+      console.log('user join', id, user.getDisplayName());
+      this.remoteTracks[id] = [];
+    });
+    this.room.on(this.jitsi.events.conference.USER_LEFT, this.onUserLeft);
+    this.room.on(this.jitsi.events.conference.TRACK_MUTE_CHANGED, track => {
+      console.log(`${track.getType()} - ${track.isMuted()}`);
+    });
+    this.room.on(
+      this.jitsi.events.conference.DISPLAY_NAME_CHANGED,
+      (userID, displayName) => console.log(`${userID} - ${displayName}`));
+    this.room.on(
+      this.jitsi.events.conference.TRACK_AUDIO_LEVEL_CHANGED,
+      (userID, audioLevel) => console.log(`${userID} - ${audioLevel}`));
+    this.room.on(
+      this.jitsi.events.conference.PHONE_NUMBER_CHANGED,
+      () => console.log(`${this.room.getPhoneNumber()} - ${this.room.getPhonePin()}`));
+    this.room.join(this.conferencePassword);
+    await this.wait(2);
+    if (this.role === 'presenter') {
+      this.room.lock(this.conferencePassword);
+    }
+  }
+
+  onConnectionFailed() {
+    console.error('Connection Failed!');
+  }
+
+  onDeviceListChanged(devices) {
+    console.info('current devices', devices);
+  }
+
+  disconnect() {
+    console.log('disconnect!');
+    if (!this.connection) {
+      return;
+    }
+    this.connection.removeEventListener(
+      this.jitsi.events.connection.CONNECTION_ESTABLISHED,
+      this.onConnectionSuccess);
+    this.connection.removeEventListener(
+      this.jitsi.events.connection.CONNECTION_FAILED,
+      this.onConnectionFailed);
+    this.connection.removeEventListener(
+      this.jitsi.events.connection.CONNECTION_DISCONNECTED,
+      this.disconnect);
+  }
+
+  unload() {
+    for (let i = 0; i < this.localTracks.length; i++) {
+      this.localTracks[i].dispose();
+    }
+    this.localTracks = [];
+    if (this.room) {
+      this.room.leave();
+      this.room = null;
+    }
+
+    if (this.connection) {
+      this.connection.disconnect();
+      this.connection = null;
+    }
+    this.remoteTracks = {};
+    this.isJoined = false;
+  }
+
+  switchVideo() {
+    this.isVideo = !this.isVideo;
+    if (this.localTracks[1]) {
+      this.localTracks[1].dispose();
+      this.localTracks.pop();
+    }
+    this.jitsi.createLocalTracks({
+      devices: [this.isVideo ? 'video' : 'desktop']
+    })
+      .then(tracks => {
+        this.localTracks.push(tracks[0]);
+        this.localTracks[1].addEventListener(
+          this.jitsi.events.track.TRACK_MUTE_CHANGED,
+          () => console.log('local track muted'));
+        this.localTracks[1].addEventListener(
+          this.jitsi.events.track.LOCAL_TRACK_STOPPED,
+          () => console.log('local track stoped'));
+        this.localTracks[1].attach($('#localVideo1')[0]);
+        this.room.addTrack(this.localTracks[1]);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  changeAudioOutput(selected) {
+    this.jitsi.mediaDevices.setAudioOutputDevice(selected.value);
+  }
+
+  startAsPresenter = async () => {
+    console.log('Start as presenter');
+    this.role = 'presenter';
+    await this.wait(1);
+    this.main();
+  }
+
+  startAsUser = async () => {
+    console.log('Start as user');
+    this.role = 'user';
+    await this.wait(1);
+    this.main();
+  }
+
+
+  main = async () => {
+    $(window).bind('beforeunload', this.unload);
+    $(window).bind('unload', this.unload);
+
     this.jitsi.init(this.initOptions);
     this.jitsi.setLogLevel(this.jitsi.logLevels.ERROR);
-    this.connection = this.createConnection(this.options);
-    this.setConnectionListeners(this.connection);
+
+    await this.wait(1);
+
+    this.connection = new this.jitsi.JitsiConnection(null, null, this.optionsJitsi);
+
+    this.connection.addEventListener(
+      this.jitsi.events.connection.CONNECTION_ESTABLISHED,
+      this.onConnectionSuccess);
+    this.connection.addEventListener(
+      this.jitsi.events.connection.CONNECTION_FAILED,
+      this.onConnectionFailed);
+    this.connection.addEventListener(
+      this.jitsi.events.connection.CONNECTION_DISCONNECTED,
+      this.disconnect);
+
+    this.jitsi.mediaDevices.addEventListener(
+      this.jitsi.events.mediaDevices.DEVICE_LIST_CHANGED,
+      this.onDeviceListChanged);
+
+    this.connection.connect();
+
+    $('#login').remove();
+
+    if (this.role !== 'presenter') {
+      $('#controls').remove();
+      return;
+    }
 
     const tracks = await this.jitsi.createLocalTracks({ devices: ['audio', 'video'] });
     this.onLocalTracks(tracks);
-  }
 
-  // initializeVideo() {
-  //   this.jitsi.init(this.initOptions);
-  //   this.jitsi.setLogLevel(this.jitsi.logLevels.ERROR);
-  //   this.connection = this.createConnection(this.options);
-  //   this.setConnectionListeners(this.connection);
-  //   this.createRoom(this.connection, this.confOptions);
-  //   this.setRoomListeners(this.room);
-  //   this.room.join();
-  // }
+    if (this.jitsi.mediaDevices.isDeviceChangeAvailable('output')) {
+      this.jitsi.mediaDevices.enumerateDevices(devices => {
+        const audioOutputDevices
+          = devices.filter(d => d.kind === 'audiooutput');
+
+        if (audioOutputDevices.length > 1) {
+          $('#audioOutputSelect').html(
+            audioOutputDevices
+              .map(
+                d =>
+                  `<option value="${d.deviceId}">${d.label}</option>`)
+              .join('\n'));
+
+          $('#audioOutputSelectWrapper').show();
+        }
+      });
+    }
+  }
 
   ngOnInit() {
 
